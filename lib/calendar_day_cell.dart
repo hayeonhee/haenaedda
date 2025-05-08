36bb527efafb70163fb4821a27773d82770fc3af
hayeonhee <may.yh.ha@gmail.com>
2025-05-08 17:22:07 +0900

    feat: remove unused parameters

--- lib/calendar_day_cell.dart
+++ lib/calendar_day_cell.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
 class CalendarDayCell extends StatelessWidget {
-  final double width;
-  final double height;
   final String dayText;
   final bool hasRecord;
   final VoidCallback onTap;
 
   const CalendarDayCell({
     super.key,
-    required this.width,
-    required this.height,
     required this.dayText,
     required this.hasRecord,
     required this.onTap,
@@ -20,16 +16,17 @@ class CalendarDayCell extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-          color: hasRecord ? Colors.blue : Colors.white,
-          border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.circular(8),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+            color: hasRecord ? Colors.deepPurple : Colors.white,
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          alignment: Alignment.topLeft,
+          child: Text(dayText),
         ),
-        alignment: Alignment.topLeft,
-        child: Text(dayText),
       ),
     );
   }
