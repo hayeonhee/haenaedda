18c33984356e7e32ffffa598214671e382770ee1
hayeonhee <may.yh.ha@gmail.com>
2025-05-12 21:43:49 +0900

    refactor: extract method and list to record_provider

--- lib/my_calendar_page.dart
+++ lib/my_calendar_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:haenaedda/calendar_screen.dart';
 import 'package:haenaedda/main.dart';
 import 'package:haenaedda/model/calendar_grid_layout.dart';
+import 'package:haenaedda/record_provider.dart';
 
 class MyCalendarPage extends StatefulWidget {
   final String title;
@@ -17,27 +19,13 @@ class _MyCalendarPageState extends State<MyCalendarPage> {
   // TODO: Change app language based on user's device settings
   final daysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
-  Map<String, Set<DateTime>> recordsByGoal = {};
-
-  void toggleDate(String goal, DateTime date) {
-    final records = recordsByGoal[goal] ?? <DateTime>{};
-
-    setState(() {
-      if (records.contains(date)) {
-        records.remove(date);
-      } else {
-        records.add(date);
-      }
-      recordsByGoal[goal] = records;
-    });
-  }
-
   // TODO: Always display the current month in this version
   DateTime focusedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    final selectedDates = recordsByGoal[kUserGoal] ?? {};
+    final recordProvider = context.watch<RecordProvider>();
+    final selectedDates = recordProvider.getRecords(kUserGoal);
     final dateLayout = CalendarGridLayout(focusedDate);
 
     return Scaffold(
@@ -70,8 +58,9 @@ class _MyCalendarPageState extends State<MyCalendarPage> {
             CalendarScreen(
               dateLayout: dateLayout,
               selectedDates: selectedDates,
-              onCellTap: (selectedDate) => toggleDate(kUserGoal, selectedDate),
-            )
+              onCellTap: (selectedDate) =>
+                  recordProvider.toggleRecord(kUserGoal, selectedDate),
+            ),
           ],
         ),
       ),
