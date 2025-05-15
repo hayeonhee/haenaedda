8b7bf0320d9e6c49d06f54dce54c3eeedf332866
hayeonhee <may.yh.ha@gmail.com>
2025-05-15 20:11:29 +0900

    refactor: convert widget to Stateful and initialize records

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,46 @@ import 'package:haenaedda/constants/app_theme.dart';
 import 'package:haenaedda/my_calendar_page.dart';
 import 'package:haenaedda/record_provider.dart';
 
-// TODO: Let the user set a goal
-String kUserGoal = 'User\'s goal';
-
 void main() {
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => RecordProvider()),
-    ],
-    child: const Haenaedda(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RecordProvider()),
+      ],
+      child: const Haenaedda(),
+    ),
+  );
 }
 
-class Haenaedda extends StatelessWidget {
+class Haenaedda extends StatefulWidget {
   const Haenaedda({super.key});
 
   @override
+  State<Haenaedda> createState() => _HaenaeddaState();
+}
+
+class _HaenaeddaState extends State<Haenaedda> {
+  String? goalId;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() async {
+      final recordProvider =
+          Provider.of<RecordProvider>(context, listen: false);
+      final goal = await recordProvider.initializeAndGetGoal();
+      setState(() {
+        goalId = goal.id;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final recordProvider = Provider.of<RecordProvider>(context, listen: false);
-    recordProvider.loadRecords();
+    if (goalId == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    final String nonNullableGoalId = goalId!;
 
     return MaterialApp(
       builder: (context, child) {
