f6532f2440795585b7c02806ec70acb66d93021d
hayeonhee <may.yh.ha@gmail.com>
2025-05-18 10:28:56 +0900

    feat: add SafeArea to prevent UI clipping

--- lib/my_calendar_page.dart
+++ lib/my_calendar_page.dart
@@ -28,39 +28,41 @@ class _MyCalendarPageState extends State<MyCalendarPage> {
     final dateLayout = CalendarGridLayout(focusedDate);
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 80),
-            Text(
-              '${focusedDate.year}년 ${focusedDate.month}월',
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: List.generate(daysOfWeek.length, (index) {
-                return Expanded(
-                  child: Text(
-                    daysOfWeek[index],
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Theme.of(context).colorScheme.onSurface,
-                      fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              Text(
+                '${focusedDate.year}년 ${focusedDate.month}월',
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: List.generate(daysOfWeek.length, (index) {
+                  return Expanded(
+                    child: Text(
+                      daysOfWeek[index],
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Theme.of(context).colorScheme.onSurface,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                );
-              }),
-            ),
-            const SizedBox(height: 10),
-            CalendarScreen(
-              dateLayout: dateLayout,
-              selectedDates: selectedDates,
-              onCellTap: (selectedDate) =>
-                  recordProvider.toggleRecord(widget.goalId, selectedDate),
-            ),
-          ],
+                  );
+                }),
+              ),
+              const SizedBox(height: 10),
+              CalendarScreen(
+                dateLayout: dateLayout,
+                selectedDates: selectedDates,
+                onCellTap: (selectedDate) =>
+                    recordProvider.toggleRecord(widget.goalId, selectedDate),
+              ),
+            ],
+          ),
         ),
       ),
     );
