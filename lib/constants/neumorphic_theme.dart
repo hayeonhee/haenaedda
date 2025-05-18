2daf538526f7c7dc30328b007f01be51e7cb2ab6
hayeonhee <may.yh.ha@gmail.com>
2025-05-18 10:34:38 +0900

    style: add styling constants for UI elements

--- lib/constants/neumorphic_theme.dart
+++ lib/constants/neumorphic_theme.dart
@@ -1,8 +1,66 @@
 import 'package:flutter/material.dart';
 
 class NeumorphicTheme {
-  static Color shadowColor = Colors.grey.shade500;
-  static const double borderRadius = 8.0;
-  static const Offset lightShadowOffset = Offset(-4, -4);
-  static const Offset darkShadowOffset = Offset(4, 4);
+  static const double blurRadius = 8;
+  static const Offset topLeftShadowOffset = Offset(-4, -4);
+  static const Offset bottomRightShadowOffset = Offset(4, 4);
+  static BorderRadius defaultBorderRadius = BorderRadius.circular(12);
+
+  BoxDecoration goalBoxDecoration(BuildContext context) {
+    return BoxDecoration(
+      color: Theme.of(context).colorScheme.background,
+      borderRadius: defaultBorderRadius,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.05),
+          offset: bottomRightShadowOffset,
+          blurRadius: blurRadius,
+        ),
+        BoxShadow(
+          color: Colors.white.withOpacity(0.7),
+          offset: topLeftShadowOffset,
+          blurRadius: blurRadius,
+        ),
+      ],
+    );
+  }
+
+  static BoxDecoration recordedCellDecoration(BuildContext context) {
+    return BoxDecoration(
+      // TODO: Allow user to customize cell color
+      color: const Color(0xFFECE7E0),
+      borderRadius: defaultBorderRadius,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.white.withOpacity(0.7),
+          offset: topLeftShadowOffset,
+          blurRadius: blurRadius,
+        ),
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          offset: bottomRightShadowOffset,
+          blurRadius: blurRadius,
+        ),
+      ],
+    );
+  }
+
+  static BoxDecoration unrecordedCellDecoration(BuildContext context) {
+    return BoxDecoration(
+      color: Theme.of(context).colorScheme.background,
+      borderRadius: defaultBorderRadius,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.white.withOpacity(0.7),
+          offset: const Offset(2, 2),
+          blurRadius: blurRadius,
+        ),
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          offset: const Offset(-2, -2),
+          blurRadius: blurRadius,
+        ),
+      ],
+    );
+  }
 }
