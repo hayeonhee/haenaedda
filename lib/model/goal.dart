24c4f0e08fa3aa88cf0c40cf7f8f3504d98f8578
hayeonhee <may.yh.ha@gmail.com>
2025-05-19 17:51:10 +0900

    feat: Add JSON serialization support for Goal model

--- lib/model/goal.dart
+++ lib/model/goal.dart
@@ -3,4 +3,18 @@ class Goal {
   String name;
 
   Goal(this.id, this.name);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+
+  factory Goal.fromJson(Map<String, dynamic> json) {
+    return Goal(
+      json['id'] as String,
+      json['name'] as String,
+    );
+  }
 }
