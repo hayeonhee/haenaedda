c86ac1624f9bbe8ddc284846ff26b18ff09918c5
hayeonhee <may.yh.ha@gmail.com>
2025-05-19 18:36:30 +0900

    feat: add firstWhereOrNull extension

---
+++ lib/utils/extensions/iterable_extensions.dart
@@ -0,0 +1,9 @@
+extension IterableExtensions<T> on Iterable<T> {
+  /// Returns the first element that satisfies [test], or null if none found.
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}
