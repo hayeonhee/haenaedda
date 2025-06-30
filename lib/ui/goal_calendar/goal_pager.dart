3a9dfd51cd8495d6086d7e2b3f6dc082617b36d4
hayeonhee <yeoneeha@gmail.com>
2025-06-30 15:52:34 +0900

    refactor: convert GoalPager to StatelessWidget and remove animation

--- lib/ui/goal_calendar/goal_pager.dart
+++ lib/ui/goal_calendar/goal_pager.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:haenaedda/model/goal.dart';
 import 'package:haenaedda/ui/goal_calendar/goal_calendar_content.dart';
 
-class GoalPager extends StatefulWidget {
+class GoalPager extends StatelessWidget {
   final List<Goal> goals;
   final PageController controller;
   final void Function(String goalId, DateTime date) onCellTap;
@@ -18,44 +18,22 @@ class GoalPager extends StatefulWidget {
   });
 
   @override
-  State<GoalPager> createState() => _GoalPagerState();
-}
-
-class _GoalPagerState extends State<GoalPager> {
-  @override
   Widget build(BuildContext context) {
     return PageView.builder(
       scrollDirection: Axis.vertical,
-      controller: widget.controller,
+      controller: controller,
       physics: const BouncingScrollPhysics(),
-      itemCount: widget.goals.length,
+      itemCount: goals.length,
       onPageChanged: (index) {
-        if (widget.onPageChanged != null && index < widget.goals.length) {
-          widget.onPageChanged?.call(index);
+        if (onPageChanged != null && index < goals.length) {
+          onPageChanged?.call(index);
         }
       },
-      itemBuilder: (context, index) {
-        return AnimatedBuilder(
-          animation: widget.controller,
-          builder: (context, child) {
-            if (!widget.controller.hasClients ||
-                widget.controller.page == null) {
-              return child!;
-            }
-            final value = (widget.controller.page! - index).clamp(-1.0, 1.0);
-            final scale = 1 - (value.abs() * 0.1);
-            return Transform.scale(
-              scale: scale,
-              child: Opacity(opacity: 1 - value.abs() * 0.3, child: child),
-            );
-          },
-          child: GoalCalendarContent(
-            key: ValueKey(widget.goals[index].id),
-            goal: widget.goals[index],
-            onCellTap: (goalId, date) => widget.onCellTap(goalId, date),
-          ),
-        );
-      },
+      itemBuilder: (context, index) => GoalCalendarContent(
+        key: ValueKey(goals[index].id),
+        goal: goals[index],
+        onCellTap: (goalId, date) => onCellTap(goalId, date),
+      ),
     );
   }
 }
